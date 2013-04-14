b3e86172acd6e3c55a8a6637378643698d61526f
Petr Pilař <the.palmik@gmail.com>
2013-04-14 18:28:00 +0200

    Added Data.Store.Lens + relevant benchmarks. Most of the time in the DS (Lens) benchmarks is consumed by the preconstruction of the store from the list of element IDs. I am going to cleanup the internals (mainly Data.Store.Internal.Function) in the future and try to incorporate some of the ideas I have for improving the performance.

---
+++ src/Data/Store/Lens.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Data.Store.Lens
+( With(..)
+) where
+
+--------------------------------------------------------------------------------
+import           Control.Applicative ((<$>))
+import qualified Control.Lens
+--------------------------------------------------------------------------------
+import           Data.Monoid 
+import           Data.Maybe 
+--------------------------------------------------------------------------------
+import qualified Data.Store.Internal.Type     as I
+import qualified Data.Store.Internal.Function as I
+import qualified Data.Store.Selection         as I (IsSelection(resolve))
+import qualified Data.Store                   as I ()
+--------------------------------------------------------------------------------
+
+class With sel where
+    with :: I.Empty (I.Index irs ts) 
+         => sel tag krs irs ts -> Control.Lens.Lens' (I.Store tag krs irs ts v)
+                                                     (I.Store tag krs irs ts v)
+
+instance I.IsSelection sel => With sel where
+    with sel tr old = mappend (fromJust $! I.genericUpdateWithKey (\_ _ -> Nothing) ids old) <$> tr (I.genericSubset ids old)
+      where
+        ids = I.resolve sel old
+
