2d31c8d80ad53f8da1b5e8711a866ff6f60dd043
Petr Pilař <the.palmik@gmail.com>
2013-04-16 16:48:51 +0200

    Added heap profiling. The graph generated by hp2ps is very weird...

--- benchmarks/src/prof.sh
+++ benchmarks/src/prof.sh
@@ -1,3 +1,6 @@
 ghc --make -package-db=../../cabal-dev/packages-7.6.2.conf/ DSPInsert.hs -O2 -rtsopts -prof -auto-all -caf-all -fforce-recomp
-./DSPInsert +RTS -p
-cat ./DSPInsert.prof
+./DSPInsert +RTS -i0.00001 -hc -p
+hp2ps -e8in -c DSPInsert.hp
+ps2pdf DSPInsert.ps
+./DSPInsert +RTS -sstderr
+
