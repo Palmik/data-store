4bc31fbca0895f838f69802c3a4bf521933d8b34
Petr Pilař <the.palmik@gmail.com>
2013-05-12 22:37:43 +0200

    Rerun the benchmark for HiggsSet.

--- bench_HS.sh
+++ bench_HS.sh
@@ -1,5 +1,5 @@
 GHC=~/.local/applications/ghc-7.4.1/bin/ghc
 cd benchmarks/src
 $GHC --make -fforce-recomp 01HS.hs
-unbuffer ./01HS -G -o ../../bench_HS.html -u ../../bench_HS.summary | tee bench_HS.out
+unbuffer ./01HS -G -o ../../bench_HS.html -u ../../bench_HS.summary | tee ../../bench_HS.out
 
